2f073f237a5686ed5e16c99d173ce317cd63af50
AsagiriBeta <2839636492@qq.com>
2025-04-20 17:45:29 +0800

    添加对modmenu的联动

--- src/client/java/AsagiriBeta/programmablechat/client/ModEntryPoint.java
+++ src/client/java/AsagiriBeta/programmablechat/client/ModEntryPoint.java
@@ -1,10 +1,17 @@
 package AsagiriBeta.programmablechat.client;
 
 import net.fabricmc.api.ModInitializer;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 
-public class ModEntryPoint implements ModInitializer {
+public class ModEntryPoint implements ModInitializer, ModMenuApi {
     @Override
     public void onInitialize() {
         new AutoMessageMod().onInitialize();
+    }
+
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return parent -> new AutoMessageScreen();
     }
 }
